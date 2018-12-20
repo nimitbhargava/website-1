cc5ebbd0fe0c35c4bff3ba51e3e52a756e92f3f0
Natalia Tepluhina <tarya.se@gmail.com>
2018-12-20 13:12:17 +0200

    Added blog to sidebar menu

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -5,7 +5,8 @@
     @input="$emit('toggleDrawer', $event)"
     app
     class="secondary darken-2"
-    dark>
+    dark
+  >
     <v-list>
       <v-list-tile to="/about">
         <v-list-tile-title class="subheading">{{ $t("menu.about") }}</v-list-tile-title>
@@ -22,6 +23,9 @@
       <v-list-tile to="/library">
         <v-list-tile-title class="subheading">{{ $t("menu.library") }}</v-list-tile-title>
       </v-list-tile>
+      <v-list-tile href="https://dev.to/vuevixens" target="_blank">
+        <v-list-tile-title class="subheading">Blog</v-list-tile-title>
+      </v-list-tile>
       <v-list-tile href="https://vuevixens.threadless.com/" target="_blank">
         <v-list-tile-title class="subheading">{{ $t("menu.shop") }}</v-list-tile-title>
       </v-list-tile>
@@ -30,12 +34,12 @@
 </template>
 
 <script>
-  import messages from '../assets/translations/header'
+import messages from "../assets/translations/header";
 
-  export default {
-    props: ['showDrawer'],
-    i18n: {
-      messages
-    },
+export default {
+  props: ["showDrawer"],
+  i18n: {
+    messages
   }
+};
 </script>
