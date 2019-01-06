168ab6c0b3cc5e5070c09479baa67cdc7b6c40c4
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-06 17:08:40 +0200

    Formatted the code

--- pages/Form.vue
+++ pages/Form.vue
@@ -1,22 +1,33 @@
 <template>
   <v-layout class="vv-container" fill-height>
     <v-container class="indexed">
-      <v-flex xs12 >
+      <v-flex xs12>
         <h1 class="display-2 text-xs-center">Event registration form</h1>
         <v-form
           action="https://vuevixens.us7.list-manage.com/subscribe/post?u=7754f9dbbdff11682c8ff2e73&amp;id=54e8642cad"
           method="post"
-          @submit="formSubmit">
-            <v-text-field name="EMAIL" label='Email' required></v-text-field>
-            <v-text-field name="FNAME" label='Full name' required></v-text-field>
-            <v-text-field name="AFFIL" label='Affiliation'></v-text-field>
-            <v-text-field name="FOOD" label='Food preferences (if any)'></v-text-field>
-            <input type="text" name="CONF" class="hidden">
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true">
-              <input type="text" name="b_7754f9dbbdff11682c8ff2e73_54e8642cad" tabindex="-1" value>
-            </div>
-            <v-btn color="card-button secondary darken-2" dark type="submit">Submit</v-btn>
+          class="submit-form"
+          @submit="formSubmit"
+          v-model="valid">
+          <v-text-field name="EMAIL" label="Email *" required></v-text-field>
+          <v-text-field
+            name="FNAME"
+            label="Full name *"
+            required></v-text-field>
+          <v-text-field name="AFFIL" label="Affiliation"></v-text-field>
+          <v-text-field
+            name="FOOD"
+            label="Food preferences (if any)"></v-text-field>
+          <input type="text" name="CONF" class="hidden-field" />
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div class="hidden-field">
+            <input
+              type="text"
+              name="b_7754f9dbbdff11682c8ff2e73_54e8642cad"
+              tabindex="-1"
+              value/>
+          </div>
+          <v-btn color="card-button secondary darken-2" dark type="submit">Submit</v-btn>
         </v-form>
       </v-flex>
     </v-container>
@@ -27,23 +38,25 @@
 export default {
   data() {
     return {
-      email: '',
-      fName: '',
-      affiliation: '',
-      foodPreferences: '',
-      conference: '',
-      gdpr: false,
+      valid: true,
     };
   },
-  methods: {
-    formSubmit() {
-      this.email = '';
-      this.fName = '';
-      this.affiliation = '';
-      this.foodPreferences = '';
-      this.conference = '';
-      this.gdpr = false;
-    }
-  }
 };
 </script>
+
+<style scoped>
+.submit-form {
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  margin-top: 40px;
+  text-align: center;
+}
+.hidden-field {
+  position: absolute;
+  left: -5000px;
+}
+.card-button {
+  margin-top: 20px;
+}
+</style>
