a9f56a867c64a1b1da18035f2a63ef0c60e7a078
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-06 16:32:25 +0200

    Transform form to Vuetify form

--- pages/Form.vue
+++ pages/Form.vue
@@ -1,61 +1,26 @@
 <template>
-  <!-- Begin Mailchimp Signup Form -->
-  <div id="mc_embed_signup">
-    <form
-      action="https://vuevixens.us7.list-manage.com/subscribe/post?u=7754f9dbbdff11682c8ff2e73&amp;id=54e8642cad"
-      method="post"
-      id="mc-embedded-subscribe-form"
-      name="mc-embedded-subscribe-form"
-      class="validate"
-      target="_blank"
-      @submit="formSubmit"
-      novalidate>
-      <div id="mc_embed_signup_scroll">
-        <h2>Subscribe to our mailing list</h2>
-        <div class="indicates-required">
-          <span class="asterisk">*</span> indicates required
-        </div>
-        <div class="mc-field-group">
-          <label for="mce-EMAIL">
-            Email Address
-            <span class="asterisk">*</span>
-          </label>
-          <input type="email" v-model="email" name="EMAIL" class="required email" id="mce-EMAIL">
-        </div>
-        <div class="mc-field-group">
-          <label for="mce-FNAME">
-            Full Name
-            <span class="asterisk">*</span>
-          </label>
-          <input type="text" v-model="fName" name="FNAME" class="required" id="mce-FNAME">
-        </div>
-        <div class="mc-field-group">
-          <label for="mce-AFFIL">Affiliation</label>
-          <input type="text" v-model="affiliation" name="AFFIL" class id="mce-AFFIL">
-        </div>
-        <div class="mc-field-group">
-          <label for="mce-FOOD">Food Preferences</label>
-          <input type="text" v-model="foodPreferences" name="FOOD" class id="mce-FOOD">
-        </div>
-        <div class="mc-field-group">
-          <label for="mce-CONF">Conference</label>
-          <input type="text" v-model="conference" name="CONF" class id="mce-CONF">
-        </div>
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true">
-          <input type="text" name="b_7754f9dbbdff11682c8ff2e73_54e8642cad" tabindex="-1" value>
-        </div>
-        <div class="clear">
-          <input
-            type="submit"
-            value="Subscribe"
-            name="subscribe"
-            id="mc-embedded-subscribe"
-            class="button">
-        </div>
-      </div>
-    </form>
-  </div>
+  <v-layout class="vv-container" fill-height>
+    <v-container class="indexed">
+      <v-flex xs12 >
+        <h1 class="display-2 text-xs-center">Event registration form</h1>
+        <v-form
+          action="https://vuevixens.us7.list-manage.com/subscribe/post?u=7754f9dbbdff11682c8ff2e73&amp;id=54e8642cad"
+          method="post"
+          @submit="formSubmit">
+            <v-text-field name="EMAIL" label='Email' required></v-text-field>
+            <v-text-field name="FNAME" label='Full name' required></v-text-field>
+            <v-text-field name="AFFIL" label='Affiliation'></v-text-field>
+            <v-text-field name="FOOD" label='Food preferences (if any)'></v-text-field>
+            <input type="text" name="CONF" class="hidden">
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true">
+              <input type="text" name="b_7754f9dbbdff11682c8ff2e73_54e8642cad" tabindex="-1" value>
+            </div>
+            <v-btn color="card-button secondary darken-2" dark type="submit">Submit</v-btn>
+        </v-form>
+      </v-flex>
+    </v-container>
+  </v-layout>
 </template>
 
 <script>
