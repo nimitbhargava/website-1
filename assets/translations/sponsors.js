0678ee728be8a7fa2bae913a51c2478865b6e30f
Natalia Tepluhina <tarya.se@gmail.com>
2019-02-19 21:23:41 +0200

    Fixed sponsors subtitle

--- assets/translations/sponsors.js
+++ assets/translations/sponsors.js
@@ -2,23 +2,33 @@ export default {
   en: {
     title: 'Our Sponsors',
     subtitle: `We are extremely grateful to our sponsors! Below is a list of
-            our valued community sponsors who are supporting our project`
+            our valued community sponsors who are supporting our project. Would you like to sponsor us? Contact us at {mail} or learn about our Sponsorship Levels in the {presskit}`,
+    mailLink: 'info@vuevixens.org',
+    presskitLink: 'https://spark.adobe.com/page/FFczmvHR6Brpu/',
   },
   es: {
     title: 'Nuestros Sponsors',
     subtitle: `Estamos inmensamente agradecidxs a nuestros sponsors! A continuación la lista
-    		de nuestros valiosos patrocinadores comunitarios que apoyan nuestro proyecto`
+        de nuestros valiosos patrocinadores comunitarios que apoyan nuestro proyecto`,
+    mailLink: 'info@vuevixens.org',
+    presskitLink: 'https://spark.adobe.com/page/FFczmvHR6Brpu/',
   },
   pt: {
     title: 'Os nossos Sponsors',
-    subtitle: `Estamos eternamente gratos aos nossos sponsor! Aqui podes encontrar a lista de membros da comunidade envolvidos com o nosso projecto.`
+    subtitle: `Estamos eternamente gratos aos nossos sponsor! Aqui podes encontrar a lista de membros da comunidade envolvidos com o nosso projecto.`,
+    mailLink: 'info@vuevixens.org',
+    presskitLink: 'https://spark.adobe.com/page/FFczmvHR6Brpu/',
   },
   pt_br: {
     title: 'Os nossos Sponsors',
-    subtitle: `Estamos eternamente gratas aos nossos patrocinadores! Aqui pode encontrar a lista de membros da comunidade envolvidos com o nosso projeto.`
+    subtitle: `Estamos eternamente gratas aos nossos patrocinadores! Aqui pode encontrar a lista de membros da comunidade envolvidos com o nosso projeto.`,
+    mailLink: 'info@vuevixens.org',
+    presskitLink: 'https://spark.adobe.com/page/FFczmvHR6Brpu/',
   },
   fr: {
     title: 'Nos Sponsors',
-    subtitle: `Nous sommes extrêmement reconnaissants envers nos sponsors ! Vous trouverez ci-dessous une liste de nos précieux sponsors communautaires qui soutiennent notre projet.`
-  }
-}
+    subtitle: `Nous sommes extrêmement reconnaissants envers nos sponsors ! Vous trouverez ci-dessous une liste de nos précieux sponsors communautaires qui soutiennent notre projet.`,
+    mailLink: 'info@vuevixens.org',
+    presskitLink: 'https://spark.adobe.com/page/FFczmvHR6Brpu/',
+  },
+};
