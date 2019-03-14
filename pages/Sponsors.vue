ab8a227fac5ab0cb5e245c7b3d093057c4bc1b2e
Natalia Tepluhina <NataliaTepluhina@users.noreply.github.com>
2019-03-14 16:52:26 +0200

    Refactored sponsors page (#48)
    * Feature/tiered sponsors idea (#47)
    
    * swapping the shop url
    
    * Added conditional displaying of the button
    
    * BoD to Advisory Board
    
    other translations for this are ok
    
    * Added styling to sponsors page and some temp markup for showing tiers
    
    needs work and temp badge image should be removed
    
    * Refactored sponsors page
    
    * Justified content to center
    
    * Removed temp gold badge
    
    * Moved lowercasing call to function body
    
    * Added margin bottom to category
    
    * Fixed margin
    
    * Added v-if on sponsors list

--- pages/Sponsors.vue
+++ pages/Sponsors.vue
@@ -2,54 +2,97 @@
   <v-layout class="vv-container" fill-height>
     <v-container class="indexed">
       <v-flex xs12 class="text-xs-center">
-        <h2 class="vv-subheading font-lato text-xs-center">{{$t('title')}}</h2>
+        <h2 class="vv-heading font-lato text-xs-center">{{ $t('title') }}</h2>
         <i18n path="subtitle" class="vv-content title" tag="p">
           <a href="mailto:info@vuevixens.org" place="mail">
-            {{
-            $t('mailLink')
-            }}
+            {{ $t('mailLink') }}
           </a>
           <a
             href="https://spark.adobe.com/page/FFczmvHR6Brpu/"
             place="presskit"
             target="blank"
-          >{{$t('presskitLink')}}</a>
+            >{{ $t('presskitLink') }}</a
+          >
         </i18n>
       </v-flex>
       <v-container grid-list-lg v-if="!!story.content">
-        <v-layout wrap>
-          <v-flex xs12 sm6 lg4 v-for="sponsor in story.content.body" :key="sponsor.name">
-            <v-card height="100%">
-              <img :src="sponsor.img" :alt="`${sponsor.name} Logo`">
-              <v-card-title justify-center>
-                <h3 class="d-block text-xs-center">{{ sponsor.name }}</h3>
-                <p class="text-xs-center">{{ sponsor.tagline }}</p>
-                <p class="text-xs-center">{{ sponsor.description }}</p>
-              </v-card-title>
-              <a :href="sponsor.website.url" class="d-block text-xs-center">
-                <i class="fa fa-link" aria-hidden="true"></i>
-                {{ sponsor.website.url }}
-              </a>
-            </v-card>
-          </v-flex>
-        </v-layout>
+        <template v-for="category in sponsors">
+          <h3
+            class="vv-subheading font-lato text-xs-center"
+            :key="category"
+            v-if="category.list && category.list.length"
+          >
+            {{ category.name }}
+          </h3>
+          <v-layout
+            v-if="category.list && category.list.length"
+            wrap
+            justify-center
+            :key="category.name"
+            class="category-wrapper"
+          >
+            <v-flex
+              xs12
+              sm6
+              lg4
+              v-for="sponsor in category.list"
+              :key="sponsor.name"
+            >
+              <v-card height="100%">
+                <img
+                  :src="sponsor.img"
+                  :alt="`${sponsor.name} Logo`"
+                  :aria-label="sponsor.name"
+                  :title="sponsor.name"
+                />
+                <v-card-title justify-center>
+                  <div class="sponsor-type" :class="sponsor.level"></div>
+                  <p class="text-xs-center">{{ sponsor.tagline }}</p>
+                  <p class="text-xs-center">{{ sponsor.description }}</p>
+                </v-card-title>
+                <a :href="sponsor.website.url" class="d-block text-xs-center">
+                  <i class="fa fa-link" aria-hidden="true"></i>
+                  {{ sponsor.website.url }}
+                </a>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </template>
       </v-container>
     </v-container>
   </v-layout>
 </template>
 
 <script>
-import storyblok from "../mixins/storyblok";
-import messages from "../assets/translations/sponsors";
+import storyblok from '../mixins/storyblok';
+import messages from '../assets/translations/sponsors';
 export default {
   mixins: [storyblok],
   i18n: {
-    messages
-  }
+    messages,
+  },
+  methods: {
+    selectSponsorsByLevel(level) {
+      level = level.toLowerCase();
+      if (level === 'partner') {
+        return this.story.content.body.filter(sponsor => !sponsor.level);
+      }
+      return this.story.content.body.filter(sponsor => sponsor.level === level);
+    },
+  },
+  computed: {
+    sponsors() {
+      const levels = ['Platinum', 'Gold', 'Silver', 'Software', 'Partner'];
+      return levels.map(level => ({
+        name: level === 'Partner' ? 'Partners' : `${level} sponsors`,
+        list: this.selectSponsorsByLevel(level),
+      }));
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 h3 {
   width: 100%;
   padding-bottom: 10px;
@@ -63,5 +106,61 @@ a {
 img {
   width: 100%;
   max-width: 100%;
+}
+
+.container {
+  &.grid-list-lg {
+    .layout {
+      .flex {
+        padding: 16px;
+      }
+
+      &:not(:only-child) {
+        margin: auto 16px;
+        margin-bottom: 20px;
+      }
+    }
+  }
+}
+
+.v-card {
+  border-radius: 0;
+  padding: 35px 20px;
+  text-align: center;
+  -moz-box-shadow: 0 0 5px #d4d4d4;
+  -webkit-box-shadow: 0 0 5px#d4d4d4;
+  box-shadow: 0 0 5px #d4d4d4;
+
+  img {
+    max-width: 200px;
+  }
+}
+
+.v-card__title {
+  padding-bottom: 0;
+}
+
+$sponsorTypes: ('platinum', '/assets/images/plat.png'),
+  ('gold', '/assets/images/gold.png'), ('silver', '/assets/images/silver.png'),
+  ('software', '/assets/images/software.png');
+
+.sponsor-type {
+  width: 50px;
+  height: 60px;
+  display: block;
+  position: absolute;
+  top: -15px;
+  left: -15px;
+
+  @each $url in $sponsorTypes {
+    &.#{nth($url,1)} {
+      background: url(nth($url, 2)) no-repeat;
+      background-size: contain;
+    }
+  }
+
+  &.silver {
+    color: rgba(0, 0, 0, 0.87);
+  }
 }
 </style>
