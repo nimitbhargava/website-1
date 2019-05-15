587d88b13f89adb3b6b82dc0d929d4ceddfe67ed
Jen Looper <jen.looper@gmail.com>
2019-05-15 11:34:26 -0400

    adding a link for 501(c)(3) status to footer

--- assets/translations/footer.js
+++ assets/translations/footer.js
@@ -1,27 +1,32 @@
 export default {
-  en: {
-    credits: 'Credits',
-    contact: 'Contact Us',
-    press: 'Press Kit'
-  },
-  es: {
-    credits: 'Créditos',
-    contact: 'Contact Us',
-    press: 'Press Kit'
-  },
-  pt: {
-    credits: 'Créditos',
-    contact: 'Contact Us',
-    press: 'Press Kit'
-  },
-  pt_br: {
-    credits: 'Créditos',
-    contact: 'Contact Us',
-    press: 'Press Kit'
-  },
-  fr: {
-    credits: 'Crédits',
-    contact: 'Contact Us',
-    press: 'Press Kit'
-  }
-}
+	en: {
+		credits: 'Credits',
+		contact: 'Contact Us',
+		press: 'Press Kit',
+		charity: 'A 501(c)(3) Public Charity',
+	},
+	es: {
+		credits: 'Créditos',
+		contact: 'Contact Us',
+		press: 'Press Kit',
+		charity: 'A 501(c)(3) Public Charity',
+	},
+	pt: {
+		credits: 'Créditos',
+		contact: 'Contact Us',
+		press: 'Press Kit',
+		charity: 'A 501(c)(3) Public Charity',
+	},
+	pt_br: {
+		credits: 'Créditos',
+		contact: 'Contact Us',
+		press: 'Press Kit',
+		charity: 'A 501(c)(3) Public Charity',
+	},
+	fr: {
+		credits: 'Crédits',
+		contact: 'Contact Us',
+		press: 'Press Kit',
+		charity: 'A 501(c)(3) Public Charity',
+	},
+};
